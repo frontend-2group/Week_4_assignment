324c28053a4feb0dadc0e3a3f02e22321092f980
kyongmee <rudal01@naver.com>
2023-07-07 01:43:47 +0900

    5번문제 진행중

--- test.js
+++ test.js
@@ -1,36 +1,49 @@
 // 4번 문제
 const $te_input = document.querySelector("#te_input");
 
-$te_input.addEventListener("focus", () => {
-    if ($te_input.value || isNaN) {
+$te_input.addEventListener("change", () => {
+    if (!isNaN($te_input.value)) {
         return alert("숫자가 입력되었습니다 안돼요");
     }
     console.log($te_input.value);
+    $te_input.value = "";
 });
 
-// function NotNum() {
-//     const regex = `/^[0-9]+$/`;
-//     if ($te_input.value.trim() === regex) {
-//         return alert("숫자가 입력되었습니다 안돼요");
-//     }
-// }
-
-//악ㄲㄲㄲㄲㄲㄲㄱ  인풋 타입 바꾸는 방법을 모르겠네..
-// 알람창 왜 계속 뜨는건데!!
+//문제점1
+//아 이러면 숫자만 입력하면 알람이 뜨는데..섞어서 쓰면 정상출렫됨....아왜에에에에에에
 
 //5번문제
 
 const $form = document.querySelector("#form");
+const $list = document.querySelector("#list");
+const $text = document.querySelector("#list_data");
+const $reset = document.querySelector("#reset");
 
 $form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log($form.content.value);
 
-    const $newContent = document.createElement("li");
-    $newContent.innerHTML = `<li>${$form.content.value}</li>`;
+    if (!$text.value.trim()) {
+        return alert("내용을 입력하세요");
+    }
 
-    $form.appendChild($newContent);
+    const $newContent = document.createElement("li");
+    $newContent.innerText = `${$form.content.value}`;
+
+    $list.appendChild($newContent);
+
+    $text.value = "";
 });
 
-//아.. 문제 1 - 빈칸도 만들어짐..
-// 문제 2 - 글이 계속 쌓임...
+// 문제  - 글이 계속 쌓임...
+// -> $text.value = "";로 해결
+
+//문제 3 - 빈칸도 입력이 가능함...알림은 뜨는데 출력도 동시에 됨
+//-> 위치를 맨앞으로 옮겨서 해결..근데 왜...?ㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+
+$reset.addEventListener("click", () => {
+    console.log($reset);
+    $list.innerHTML = `<li></li>`;
+});
+
+//초기화라....ㅠㅠ
