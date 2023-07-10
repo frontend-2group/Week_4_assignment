bd321cc58199568ca679561eaddf519db7f9f908
55555-Jyeon <jyeon380516@gmail.com>
2023-07-11 00:55:36 +0900

    level01: 진행 중

--- Javascript/level1/level.js
+++ Javascript/level1/level.js
@@ -4,57 +4,78 @@ import { BANK_LIST, ACCOUNT_FORM } from './account.js';
 // console.log(ACCOUNT_FORM);
 
 
-
-// 01. 은행선택(bank-selector) -> BANK_LIST
+// variable collection
+const bankList = Object.values(BANK_LIST);
+const accountForm = Object.values(ACCOUNT_FORM);
 
 const $bankSelector = document.querySelector('#bank-selector');
-
-$bankSelector.innerHTML = `
-<option value="0" class='bankName'> -- 은행을 선택해주세요 -- </option>
-<option value="1" class='bankName'> 성용은행 </option>
-<option value="2" class='bankName'> 사과은행 </option>
-<option value="3" class='bankName'> 코딩은행 </option>
-<option value="4" class='bankName'> 자바은행 </option>
-<option value="5" class='bankName'> 파이은행 </option>
-<option value="6" class='bankName'> 리엑은행 </option>
-<option value="7" class='bankName'> 자스은행 </option>
-<option value="8" class='bankName'> 모두은행 </option>
-`;
+const $accountInput = document.querySelector('#account-input');
+const $submitBtn = document.querySelector('.submit-button');
+const $accountList = document.querySelector('#account-list');
 
 
+// 01. 은행선택(bank-selector) -> BANK_LIST
+for(let [bankIdx, bankName] of Object.entries(BANK_LIST)){
+    const option = document.createElement('option'); 
+    option.value = bankIdx;
+    option.innerText = bankName;
+    $bankSelector.appendChild(option);
 
-// 02. 선택된 은행에 맞는 형식(ACCOUNT_FORM)으로 계좌번호 출력
+    // 은행 미선택 시 alert
+}
 
-function secureAccount($accountNum) {
-    const $accountInput = document.querySelector('#account-input');
-    const $accountList = document.querySelector('#account-list');
 
-    // 입력 받은 숫자가 12자리인지 확인
-    if ($accountInput.value.length !== 12){
-        $accountInput.value = '';
-        return alert('계좌 번호는 모두 12자리 입니다.');
+// 02-1. $accountInput에 숫자만 입력되도록 제한
+$accountInput.addEventListener('input', (e) => {
+    let inputValue = e.currentTarget.value;
+    const regExp = /\d+$/g.test(inputValue);
+
+    if(!regExp){
+        inputValue.replace(/\D/gi, '');
+        inputValue.replace(/^A-Za-z0-9/gi, '');
+        alert('숫자만 입력해주세요.');
+        inputValue = '';
+        // 문자나 특수문자 입력 시 삭제되지 않음, replace가 제대로 동작하지 않는 건가?
     }
-
-    // 12자리가 맞다면 account-list에 등록
-    const currentList = addAccount();
-    const li = document.createElement('li');
-
-    li.innerHTML = `
-        <li> ${$bankSelector.value}:  </li>
-    `
-
-    $accountList.appendChild(li);
-    $accountList.value = '';
-
-}
+});
 
 
-
-// 03. 제출 버튼 클릭 -> #account-list에 계좌 정보 등록
-function addAccount(){
-    const bankList = Object.values(BANK_LIST);
-    const accountForm = Object.values(ACCOUNT_FORM);
+// 02-2. 선택된 은행에 맞는 형식(ACCOUNT_FORM)으로 계좌번호 출력
+$submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     
-    // switch? 
+    let bankId = $bankSelector.value;
+    let bankName = bankList[bankId];
+    let accountNumber = $accountInput.value;
     
-}
+    // 입력 받은 숫자가 12자리인지 확인
+    if (accountNumber.length < 12){
+        alert('계좌번호는 모두 12자리 입니다.');
+        accountNumber = '';
+    }
+    
+    for(let [bankIdx, secureFormat] of Object.entries(ACCOUNT_FORM)){        
+        // 가운데 번호 *로 변경하기
+        function replaceToAsterisk() {
+            let parts = secureFormat.split("-");
+
+            if (parts.length > 2) {
+              for (let i = 1; i < parts.length - 1; i++) {
+                parts[i] = "*".repeat(parts[i].length);
+              }
+            }
+            return parts.join("-");
+
+        }
+        let securedNum =  replaceToAsterisk(accountNumber);  
+        
+        
+        
+        // 03. 제출 버튼 클릭 -> $accountList에 계좌 정보 등록
+        const li = document.createElement('li');
+
+        li.innerHTML = ` <li> ${bankName}: ${securedNum} </li> `;
+        $accountList.appendChild(li);
+        accountNumber = '';
+    }
+});
--- Javascript/level2/level2.js
+++ Javascript/level2/level2.js
@@ -1,8 +1,40 @@
 import { RESERVATION_LIST } from './reservation .js';
-console.log(RESERVATION_LIST);
+// console.log(RESERVATION_LIST);
 
 /* 
+
 예약 고객확인하기
 
+1. 예약 고객 리스트는 js로 이미 import 되어 console.log에 찍어두었습니다.
+2. 고객 리스트에 없는 고객정보의 경우 일치하는 항목이 없습니다라는 console.log와 함께 텍스트가 나타납니다.
+3. 이름과 핸드폰 번호 모두 일치하지 않으면 고객은 검색할 수 없습니다.
+4. 고객데이터가 있으면 고객번호가 텍스트로 나타납니다.
 
 */
+
+const reservationList = Object.values(RESERVATION_LIST);
+
+const $userName = document.querySelector('#user-name');
+const $userPhone = document.querySelector('#user-phone');
+const $reservationNumber = document.querySelector('#reservation-number');
+const $confirmBtn = document.querySelector('#confirm-button');
+
+// 고객 리스트에 없는 고객정보의 경우:  "일치하는 항목이 없습니다" -> console.log & alert
+$confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let findName = reservationList.filter((person) => person.name === $userName.value );
+    let findPhone = findName.find((person) => person.phone === $userPhone.value);
+    let reservationNumber = reservationList.find((person) => person.name === $userName.value && person.phone === $userPhone.value);
+    
+
+    if(findName = ''){
+        alert(`${$userName.value}로 예약된 내역이 없습니다. 다시 한 번 확인해주세요.`);
+        $userName.value = '';
+    } else if(findName && findPhone == undefined){
+        alert(`입력하신 번호로 예약된 내역이 없습니다. 다시 한 번 확인해주세요.`);
+        $userPhone.value = '';
+    } else {
+        $reservationNumber.innerText = `${reservationNumber.number}`
+    }
+})
